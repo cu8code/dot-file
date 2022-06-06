c06f91eb8856a4dc82f263014e087428332b1c59
ankanroy-code <8ankanroy@gmail.com>
2022-06-06 23:19:58 +0530

    added treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,8 @@
 require "user.options" 
 require "user.keymaps" 
-require "user.plugins" 
-require "user.cmp" 
-require "user.nvim-tree" 
-require "user.vim-bbye" 
-require "user.lualine" 
-require "user.toggleterm" 
-require "user.nvim-autopairs" 
-require "user.nvim-comment" 
-require "user.buffer-line" 
+require "user.plugins"
+
+require "user.treesitter"
 
 vim.g.tokyonight_style = "night"
 vim.cmd [[ colorscheme tokyonight 
