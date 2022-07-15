dd64f81ce85cba062aefa6e9736964888b04b136
ankanroy-code <8ankanroy@gmail.com>
2022-07-15 21:26:08 +0530

    remve git

--- emacs.d/elpa/base16-theme-20220621.905/base16-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "base16-theme" "20220621.905" "Collection of themes built on combinations of 16 base colors" 'nil :commit "ecc3539f61f0c58091d522cf88bd86be748ca965" :authors
-  '(("Kaleb Elwert" . "belak@coded.io")
-    ("Neil Bhakta"))
-  :maintainer
-  '("Kaleb Elwert" . "belak@coded.io")
-  :url "https://github.com/base16-project/base16-emacs")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
