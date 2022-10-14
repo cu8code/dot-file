42be670388b12ff2696d7b5566626d8cd9afe59e
cu8code <git8ankanroy@gmail.com>
2022-10-14 16:08:50 +0530

    removed more stuff

--- scripts/alias.zsh
+++ scripts/alias.zsh
@@ -1,5 +1,5 @@
 alias n="kitty -e nvim"
 alias g=lazygit;
 typeset -F SECONDS
-export TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
+# export TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
 export LUTRIS_SKIP_INIT=false
